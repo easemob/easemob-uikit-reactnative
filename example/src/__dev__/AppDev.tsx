37f8a4d4a86b9cce5faed1079e3e92b785cba3ba
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-11-28 16:13:40 +0800

    chore: uikit: update ui component

--- example/src/__dev__/AppDev.tsx
+++ example/src/__dev__/AppDev.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View } from 'react-native';
 
-import { default as Test } from './test_alert';
+import { default as Test } from './test_modal';
 
 // if (
 //   Platform.OS === 'android' &&
