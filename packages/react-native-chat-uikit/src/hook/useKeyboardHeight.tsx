bbd6af9a83448dfd09a9e09c44f424eeac48bf8f
asteriskzuo <zuoyuhsywn@hotmail.com>
2023-12-14 17:48:20 +0800

    chore: uikit: update keyboard height

--- packages/react-native-chat-uikit/src/hook/useKeyboardHeight.tsx
+++ packages/react-native-chat-uikit/src/hook/useKeyboardHeight.tsx
@@ -1,24 +1,40 @@
 import * as React from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, Platform } from 'react-native';
 
 /**
  * Get the keyboard height. Need to be obtained dynamically.
  */
 export function useKeyboardHeight() {
   const [keyboardHeight, setKeyboardHeight] = React.useState(0);
+  const [keyboardCurrentHeight, setKeyboardCurrentHeight] = React.useState(0);
 
   React.useEffect(() => {
+    const showSubscriptionI = Keyboard.addListener('keyboardWillShow', (e) => {
+      if (Platform.OS === 'ios') {
+        setKeyboardHeight(e.endCoordinates.height);
+      }
+    });
+    const hideSubscriptionI = Keyboard.addListener('keyboardWillHide', () => {
+      if (Platform.OS === 'ios') {
+      }
+    });
     const showSubscription = Keyboard.addListener('keyboardDidShow', (e) => {
-      setKeyboardHeight(e.endCoordinates.height);
+      if (Platform.OS === 'android') {
+        setKeyboardHeight(e.endCoordinates.height);
+      }
+      setKeyboardCurrentHeight(e.endCoordinates.height);
     });
-    const hideSubscription = Keyboard.addListener('keyboardWillHide', () => {
-      // setKeyboardHeight(0);
+    const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardCurrentHeight(0);
     });
+
     return () => {
+      showSubscriptionI.remove();
+      hideSubscriptionI.remove();
       showSubscription.remove();
       hideSubscription.remove();
     };
-  }, [setKeyboardHeight]);
+  }, []);
 
-  return keyboardHeight;
+  return { keyboardHeight, keyboardCurrentHeight };
 }
