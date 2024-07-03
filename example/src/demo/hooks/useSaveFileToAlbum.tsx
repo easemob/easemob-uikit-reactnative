a83f3a763c1fa04cf381cf418b4573b0de883bfe
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-07-03 19:13:45 +0800

    chore: example: update for agora

--- example/src/demo/hooks/useSaveFileToAlbum.tsx
+++ example/src/demo/hooks/useSaveFileToAlbum.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import {
   generateFileName,
   getFileExtension,
@@ -6,7 +7,7 @@ import {
   Services,
   useSaveFile,
   uuid,
-} from 'react-native-chat-uikit';
+} from '../../rename.uikit';
 
 export function useSaveFileToAlbum() {
   const { getDocumentPath } = useSaveFile();
